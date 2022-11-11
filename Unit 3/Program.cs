6a2b84e8ffd5f772558d522c948549bda0e39fdd
SHUGAMEN <i.sugatov@tps.stg.ru>
2022-11-11 14:30:19 +0600

    Добавление символов, знаков

--- Unit 3/Program.cs
+++ Unit 3/Program.cs
@@ -1,9 +1,15 @@
-﻿static void Main(string[] args)
+﻿using System;
+class MainClass
 {
-    const string MyName = "Ivan";
-    Console.WriteLine(MyName);
-    Console.WriteLine("\n Привет, мир");
-    Console.WriteLine("\n Мне 28 лет");
-    Console.WriteLine("My name is \t Ivan");
-    Console.ReadKey();
+    static void Main(string[] args)
+    {
+        const string MyName = "Ivan";
+        Console.WriteLine(MyName);
+        Console.WriteLine("\n Привет, мир");
+        Console.WriteLine("\n Мне 28 лет");
+        Console.WriteLine("My name is \t Ivan");
+        Console.WriteLine("\u0040");
+        Console.WriteLine("\x23");
+        Console.ReadKey();
+    }
 }
